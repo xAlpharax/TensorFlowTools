5394bb2bc4154d4fe539dd7821a7396b22939566
xAlpharax <iustin.raznic@gmail.com>
2020-07-12 03:17:47 +0300

    Added BatchAttNorm

--- layeroperations.py
+++ layeroperations.py
@@ -110,6 +110,69 @@ class InstanceNormalization(Layer):
 
 #######################################################################################
 
+import tensorflow.keras.backend as K
+from tensorflow.keras import layers
+from tensorflow.keras.initializers import Constant
+
+class BatchAttNorm(layers.BatchNormalization):
+    def __init__(self, momentum=0.99, epsilon=0.001, axis=-1, **kwargs):
+        super(BatchAttNorm, self).__init__(momentum=momentum, epsilon=epsilon, axis=axis, center=False, scale=False, **kwargs)
+        
+        if self.axis == -1:
+            self.data_format = 'channels_last'
+        else:
+            self.data_format = 'channel_first'
+        
+    def build(self, input_shape):
+        if len(input_shape) != 4:
+            raise ValueError('expected 4D input (got {}D input)'.format(input_shape))
+                
+        super(BatchAttNorm, self).build(input_shape)
+        
+        dim = input_shape[self.axis]
+        shape = (dim, )
+        
+        self.GlobalAvgPooling = layers.GlobalAveragePooling2D(self.data_format)
+        self.GlobalAvgPooling.build(input_shape)
+    
+        self.weight = self.add_weight(name='weight',
+                                      shape=shape,
+                                      initializer=Constant(1),
+                                      trainable=True)
+
+        self.bias = self.add_weight(name='bias',
+                                    shape=shape,
+                                    initializer=Constant(0),
+                                    trainable=True)
+
+        self.weight_readjust = self.add_weight(name='weight_readjust',
+                                               shape=shape,
+                                               initializer=Constant(0),
+                                               trainable=True)
+        
+        self.bias_readjust = self.add_weight(name='bias_readjust',
+                                             shape=shape,
+                                             initializer=Constant(-1),
+                                             trainable=True)
+        
+
+    def call(self, input):
+        
+        avg = self.GlobalAvgPooling(input)
+        attention = K.sigmoid(avg * self.weight_readjust + self.bias_readjust)
+
+        bn_weights = self.weight * attention
+        
+        out_bn = super(BatchAttNorm, self).call(input)
+        
+        if K.int_shape(input)[0] is None or K.int_shape(input)[0] > 1:
+            bn_weights = bn_weights[:, None, None, :]
+            self.bias  = self.bias[None, None, None, :]
+
+        return out_bn * bn_weights + self.bias
+
+#######################################################################################
+
 def conv2d(layer_input, filters, f_size=5, dropout_rate=0):
 	"""Layers used during downsampling"""
 	d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same')(layer_input)
